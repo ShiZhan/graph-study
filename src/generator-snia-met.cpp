029fe36a2100506f384813c2ad73c5890e524d35
ShiZhan <g.shizhan.g@gmail.com>
2014-11-28 18:10:40 +0800

    generate edge list from block trace (SNIA Microsoft Exchange-Server Traces)

--- src/generator-snia-met.cpp
+++ src/generator-snia-met.cpp
@@ -2,23 +2,26 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <list>
 #include <string>
 
 int main (int argc, char* argv[]) {
 	using namespace std;
 
-	int diffuse = 0;
+	unsigned short diffuse_factor = 0;
+	unsigned short block_scale = 16;
 	string filename;
 
-	if (argc < 3) {
-		cerr << "generator <trace.csv> <diffuse factor>" << endl;
+	if (argc < 4) {
+		cerr << "generator <trace.csv> <diffuse factor> <block scale>" << endl;
 		return -1;
 	}
 
 	try {
 		filename = argv[1];
-        size_t lastChar;
-        diffuse = stoi(argv[2], &lastChar, 10);
+		size_t lastChar;
+		diffuse_factor = stoi(argv[2], &lastChar, 10);
+		block_scale = stoi(argv[3], &lastChar, 10);
 	} catch(exception& e) {
 		cout << e.what() << endl;
 		return -1;
@@ -29,18 +32,36 @@ int main (int argc, char* argv[]) {
 
     string line;
     vector<string> tokens;
-
+	list<unsigned long long> neighbours;
+	bool skip = true;
 	while (getline(infile, line)) {
-		stringstream ss(line);
+		stringstream splitter(line);
 		string token;
 		tokens.clear();
-        while(getline(ss, token, ',')) tokens.push_back(token);
+        while(getline(splitter, token, ','))
+			tokens.push_back(token.erase(0, token.find_last_of(" \t") + 1));
 
-		if (tokens.size() < 15) continue;
-
-		for (auto token: tokens) cout << token << '|';
-
-		cout << "\n----------------------" << endl;
+		string op = tokens.at(0);
+		if (skip) {
+			if (op == "EndHeader") skip = false;
+		} else {
+			if ((op == "DiskRead" || op == "DiskWrite") && tokens.size() == 15) {
+				unsigned long long offset = 0;
+				unsigned long length = 0, disknum = 0;
+				try {
+					offset = stoull(tokens.at(5), nullptr, 16);
+					length = stoul(tokens.at(6), nullptr, 16);
+					disknum = stoul(tokens.at(8), nullptr, 10);
+				} catch(exception& e) {
+					cout << e.what() << endl;
+				}
+				unsigned long long block_id = offset >> block_scale;
+				for (auto n: neighbours) cout << n << "-" << block_id << ", ";
+				cout << endl;
+				neighbours.push_front(block_id);
+				if (neighbours.size() > diffuse_factor) neighbours.pop_back();
+			}
+		}
 	}
 
 	return 0;
