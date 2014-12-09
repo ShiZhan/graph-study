6eb369d016a831f277cfba78719589afd97964a6
ShiZhan <g.shizhan.g@gmail.com>
2014-12-09 11:37:09 +0800

    no need to sort here

--- scripts/count-trace-edges.sh
+++ scripts/count-trace-edges.sh
@@ -1,1 +1,1 @@
-gunzip -c $1 | sed -e '/BeginHeader/,/EndHeader/d' | grep "Disk[Read|Write]" | awk -F',' '{print $6}' | Debug/sequence-to-graph.exe $2 $3 | sort -n | uniq -c | sort -n -r -k 1 -t ' ' | wc -l
+gunzip -c $1 | sed -e '/BeginHeader/,/EndHeader/d' | grep "Disk[Read|Write]" | awk -F',' '{print $6}' | Debug/sequence-to-graph.exe $2 $3 | sort -n | uniq -c | wc -l
