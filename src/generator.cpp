5e9dc23ba59b7e7ca46243e8c6ca0cc3c645e972
ShiZhan <g.shizhan.g@gmail.com>
2014-12-17 21:43:16 +0800

    comment

--- src/generator.cpp
+++ src/generator.cpp
@@ -12,6 +12,7 @@ int main (int argc, char* argv[]) {
 		return -1;
 	}
 
+	// get vertex scale and edge multiplier (average degree)
 	unsigned short v_factor = 10;
 	unsigned short e_factor = 8;
 
@@ -24,6 +25,7 @@ int main (int argc, char* argv[]) {
 		return -1;
 	}
 
+	// get seed matrix (4 ranges)
 	int seed[4] = {57, 19, 19, 5};
 
 	if (argc > 3) {
@@ -48,15 +50,18 @@ int main (int argc, char* argv[]) {
 
 	srand((unsigned int)time(NULL));
 
-	unsigned long long V = 1 << v_factor;
-	unsigned long long E = V * e_factor;
+	unsigned long long V = 1 << v_factor; // 2^v_factor vertices
+	unsigned long long E = V * e_factor;  // 2^v_factor * e_factor (average degree) edges
 	int A = seed[0], B = A + seed[1], C = B + seed[2], D = C + seed[3];
 
 	for (unsigned long long e = 0; e < E; e++) {
 		unsigned long long u = 0, v = 0;
 		for (unsigned short i = 0; i < v_factor; i++) {
-			int roll = rand() % D;
+			int roll = rand() % D; // roll i times, 2^i vertices
 			unsigned long long u1 = u << 1, v1 = v << 1;
+			// A|B
+			// ---
+			// C|D
 			if (roll < A) {u = u1; v = v1;}
 			else if (roll < B) {u = u1 + 1; v = v1;}
 			else if (roll < C) {u = u1; v = v1 + 1;}
