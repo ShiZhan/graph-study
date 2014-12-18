70901713affbc284ca0a2c68e48e14126110e6ac
ShiZhan <g.shizhan.g@gmail.com>
2014-12-18 17:37:59 +0800

    use scale and average degree as parameters directly

--- src/generator.cpp
+++ src/generator.cpp
@@ -8,21 +8,21 @@ int main (int argc, char* argv[]) {
 	using namespace std;
 
 	// default values
-	unsigned short v_factor = 10;
-	unsigned short e_factor = 8;
+	unsigned short scale = 10;
+	unsigned short degree = 8;
 	int seed[4] = {57, 19, 19, 5};
 
 	if (argc < 3) {
-		cerr << "generator <vertex(" << v_factor << ")> <edge(" << e_factor
+		cerr << "generator <scale(" << scale << ")> <degree(" << degree
 			<< ")> [seed(" << seed[0] << ":" << seed[1] << ":" << seed[2] << ":" << seed[3] << ")]" << endl;
 		return -1;
 	}
 
-	// get vertex scale and edge multiplier (average degree)
+	// get vertex scale and average degree
 	try {
 		size_t lastChar;
-		v_factor = stoi(argv[1], &lastChar, 10);
-		e_factor = stoi(argv[2], &lastChar, 10);
+		scale = stoi(argv[1], &lastChar, 10);
+		degree = stoi(argv[2], &lastChar, 10);
 	} catch(exception& e) {
 		cerr << e.what() << endl;
 		return -1;
@@ -45,13 +45,13 @@ int main (int argc, char* argv[]) {
 
 	srand((unsigned int)time(NULL));
 
-	unsigned long long V = 1 << v_factor; // 2^v_factor vertices
-	unsigned long long E = V * e_factor;  // 2^v_factor * e_factor edges
+	unsigned long long V = 1 << scale;      // 2^scale vertices
+	unsigned long long E = V * degree / 2;  // 2^scale * degree / 2 edges
 	int A = seed[0], B = A + seed[1], C = B + seed[2], D = C + seed[3];
 	// generate edge list
 	for (unsigned long long e = 0; e < E; e++) {
 		unsigned long long u = 0, v = 0;
-		for (unsigned short i = 0; i < v_factor; i++) {
+		for (unsigned short i = 0; i < scale; i++) {
 			int roll = rand() % D; // roll dice
 			u <<= 1; v <<= 1;
 			// (0, 0) A | (0, 1) B
