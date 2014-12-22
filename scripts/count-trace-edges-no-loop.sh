89977e358b22dc28d8bf866a2d4a1d4464bfcbd6
ShiZhan <g.shizhan.g@gmail.com>
2014-12-22 11:22:00 +0800

    run script everywhere

--- scripts/count-trace-edges-no-loop.sh
+++ scripts/count-trace-edges-no-loop.sh
@@ -1,1 +1,2 @@
-gunzip -c $1 | sed '/BeginHeader/,/EndHeader/d;/^\s\+Disk[Read|Write]/!d' | awk 'BEGIN {FS=","} {print $6}' | Debug/sequence-to-graph.exe $2 $3 $4 $5 | awk '{if ($1!=$2) print $0}' | sort -n | uniq -c | wc -l
+export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
+gunzip -c $1 | sed '/BeginHeader/,/EndHeader/d;/^\s\+Disk[Read|Write]/!d' | awk 'BEGIN {FS=","} {print $6}' | $SCRIPT_DIR/../Debug/sequence-to-graph.exe $2 $3 $4 $5 | awk '{if ($1!=$2) print $0}' | sort -n | uniq -c | wc -l
--- scripts/rmat-no-loop-uniq.sh
+++ scripts/rmat-no-loop-uniq.sh
@@ -1,1 +1,2 @@
-Debug/generator.exe $@ | awk '{if ($1!=$2) print $0}' | sort -n | uniq -u
+export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
+$SCRIPT_DIR/../Debug/generator.exe $@ | awk '{if ($1!=$2) print $0}' | sort -n | uniq -u
