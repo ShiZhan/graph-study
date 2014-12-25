2d833b9b08d76a68f6301fa2d1c9d709c9910296
ShiZhan <g.shizhan.g@gmail.com>
2014-12-25 23:25:08 +0800

    portability

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <time.h>
 #include "options.h"
 
@@ -13,17 +12,21 @@ int main (int argc, char* argv[]) {
 	int seed[4] = {57, 19, 19, 5};
 
 	if (chkOption(argv, argv + argc, "-h")) {
-		cout << "generator -s [scale] -d [degree] -m [seed matrix]" << endl;
-		cout << " -h:     \t ask for help" << endl;
-		cout << " -s (" << scale  << "):\t 2^scale vertices" << endl;
-		cout << " -d (" << degree << "):\t average degree" << endl;
-		cout << " -m (" << seed[0] << ":" << seed[1] << ":" << seed[2] << ":" << seed[3] << "):\t seed matrix" << endl;
+		cout << "generator -flag [option]" << endl;
+		cout << " -h:\t ask for help" << endl;
+		cout << " -s:\t (" << scale  << ") 2^scale vertices" << endl;
+		cout << " -d:\t (" << degree << ") average degree" << endl;
+		cout << " -m:\t (" << seed[0] << ":" << seed[1] << ":" << seed[2] << ":" << seed[3] << ") seed matrix" << endl;
+		cout << " -n:\t no loop edges" << endl;
+		cout << " -u:\t undirected" << endl;
 		return 0;
 	}
 
 	char* scale_str  = getOption(argv, argv + argc, "-s"); 
 	char* degree_str = getOption(argv, argv + argc, "-d"); 
 	char* seed_str   = getOption(argv, argv + argc, "-m"); 
+	bool  noloop     = chkOption(argv, argv + argc, "-n"); 
+	bool  undirected = chkOption(argv, argv + argc, "-u"); 
 
 	// get vertex scale
 	if (scale_str) {
@@ -41,8 +44,13 @@ int main (int argc, char* argv[]) {
 
 	// get seed matrix (4 ranges)
 	if (seed_str) {
-		try { sscanf_s(seed_str, "%u:%u:%u:%u", seed, seed+1, seed+2, seed+3); }
-		catch(exception& e) { cerr << e.what() << endl; }
+		try {
+#ifdef _MSC_VER
+			sscanf_s(seed_str, "%u:%u:%u:%u", seed, seed+1, seed+2, seed+3);
+#else
+			sscanf(seed_str, "%u:%u:%u:%u", seed, seed+1, seed+2, seed+3);
+#endif
+		} catch(exception& e) { cerr << e.what() << endl; }
 	}
 
 	srand((unsigned int)time(NULL));
@@ -63,7 +71,10 @@ int main (int argc, char* argv[]) {
 			else if (roll >= B) u ++;      // [B, C)
 			else if (roll >= A) v ++;      // [A, B)
 		}
-	    cout << u << " " << v << endl;
+		if (!noloop || (u!=v)) {
+			cout << u << " " << v << endl;
+			if (undirected) cout << v << " " << u << endl;
+		}
 	}
 
 	return 0;
