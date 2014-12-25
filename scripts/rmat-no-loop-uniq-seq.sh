492268a04d3a979691dc0c6e87af9b3cca902ef9
ShiZhan <g.shizhan.g@gmail.com>
2014-12-25 23:47:11 +0800

    simplify scripts

--- scripts/rmat-no-loop-uniq-seq.sh
+++
@@ -1,2 +0,0 @@
-export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
-$SCRIPT_DIR/../Release/generator.exe $@ | awk '{if ($1!=$2) print $0}' | sort -n -k 1 -k 2 | uniq -u
--- scripts/rmat-undirected-no-loop-uniq.sh
+++ scripts/rmat-undirected-no-loop-uniq.sh
@@ -1,2 +1,2 @@
 export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
-$SCRIPT_DIR/../Release/generator.exe $@ | awk '{if ($1!=$2) print $1" "$2"\n"$2" "$1}' | sort -n | uniq -u
+$SCRIPT_DIR/../Release/generator.exe $@ -n -u | sort -n -k 1 -k 2 | uniq -u
