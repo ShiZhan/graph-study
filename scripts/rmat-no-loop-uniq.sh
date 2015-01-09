b6b6a8aca28a4596ba80fcab27852b59656b3b32
ShiZhan <g.shizhan.g@gmail.com>
2015-01-09 22:40:39 +0800

    sort -u instead of uniq

--- scripts/rmat-no-loop-uniq.sh
+++ scripts/rmat-no-loop-uniq.sh
@@ -1,2 +1,2 @@
 export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
-$SCRIPT_DIR/../Release/generator.exe $@ -n | sort -n -k 1 -k 2 | uniq
+$SCRIPT_DIR/../Release/generator.exe $@ -n | sort -n -u -k 1 -k 2
