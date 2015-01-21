75b8ab8093e75b56ff71178c8e3d52e430c516f6
ShiZhan <g.shizhan.g@gmail.com>
2015-01-21 15:14:00 +0800

    sed variable

--- scripts/get-boost.sh
+++ scripts/get-boost.sh
@@ -1,6 +1,6 @@
 export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 export INSTALL_DIR=$( pwd )
-export BOOST_DIR="$INSTALL_DIR/boost_1_57_0"
+export BOOST_DIR="$( echo ${INSTALL_DIR}/boost_1_57_0 | sed 's/\//\\\//g')"
 wget http://jaist.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz
 echo "$( tar -xzvf boost_1_57_0.tar.gz | wc -l ) files extracted."
 sed -in-place "s/BOOST_DIR = .*$/BOOST_DIR = ${BOOST_DIR}/g" $SCRIPT_DIR/../src/Makefile
