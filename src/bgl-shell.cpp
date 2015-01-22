804184e51d3e70859f7786b9b4825bf9b91bd122
ShiZhan <g.shizhan.g@gmail.com>
2015-01-23 01:02:30 +0800

    for range

--- src/bgl-shell.cpp
+++ src/bgl-shell.cpp
@@ -12,19 +12,19 @@ int main(int argc, char* argv[]) {
 
 	typedef pair<uint, uint> Edge;
 	typedef adjacency_list<setS, vecS, directedS, no_property> Graph;
-	typedef Graph::vertex_iterator    ItrVertex;
 	typedef Graph::adjacency_iterator ItrNeighbour;
 
 	if (chkOption(argv, argv + argc, "-h")) {
-		cout << "generator -flag [option]" << endl;
+		cout << "bgl-shell -flag [option]" << endl;
 		cout << " -h:\t ask for help" << endl;
 		cout << " -i:\t (cin) input edge list" << endl;
 		return 0;
 	}
 
 	char* edges_file = getOption(argv, argv + argc, "-i");
-	string line;
+
 	uint u, v, N = 0;
+	string line;
 	vector<Edge> edges;
 	if (edges_file) {
 		ifstream ifs(edges_file);
@@ -43,15 +43,13 @@ int main(int argc, char* argv[]) {
 			if (v > N) N = v;
 		}
 	}
-
 	Graph g(edges.begin(), edges.end(), ++N);
-	ItrVertex vertexIt, vertexEnd;
+
 	ItrNeighbour neighbourIt, neighbourEnd;
-	boost::tie(vertexIt, vertexEnd) = vertices(g);
-	for (; vertexIt != vertexEnd; ++vertexIt) {
-		cout << *vertexIt << " --> ";
-		boost::tie(neighbourIt, neighbourEnd) = adjacent_vertices(*vertexIt, g);
+	for (auto v: g.vertex_set()) {
+		cout << v << " --> ";
+		boost::tie(neighbourIt, neighbourEnd) = adjacent_vertices(v, g);
 		for (; neighbourIt != neighbourEnd; ++neighbourIt) cout << *neighbourIt << " ";
-		cout << "\n";
+		cout << endl;
 	}
 }
