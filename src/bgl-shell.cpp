87e222e97100015c101d08c855839af000605a40
ShiZhan <g.shizhan.g@gmail.com>
2015-01-23 10:45:31 +0800

    silly redundant code

--- src/bgl-shell.cpp
+++ src/bgl-shell.cpp
@@ -5,51 +5,59 @@
 #include "options.h"
 #include "utils.h"
 
-int main(int argc, char* argv[]) {
-	using namespace std;
-	using namespace boost;
-	using namespace opt;
+using namespace std;
+using namespace boost;
+typedef adjacency_list<setS, vecS, directedS, no_property> Graph;
 
-	typedef pair<uint, uint> Edge;
-	typedef adjacency_list<setS, vecS, directedS, no_property> Graph;
-	typedef Graph::adjacency_iterator ItrNeighbour;
-
-	if (chkOption(argv, argv + argc, "-h")) {
-		cout << "bgl-shell -flag [option]" << endl;
-		cout << " -h:\t ask for help" << endl;
-		cout << " -i:\t (cin) input edge list" << endl;
-		return 0;
-	}
-
-	char* edges_file = getOption(argv, argv + argc, "-i");
-
-	uint u, v, N = 0;
+uint get_edges(char* edges_file, Graph &g) {
+	uint u, v, total = 0;
 	string line;
-	vector<Edge> edges;
 	if (edges_file) {
 		ifstream ifs(edges_file);
 		while (getline(ifs, line)) {
 			SSCANF((line.c_str(), "%u %u", &u, &v));
-			edges.push_back(Edge(u, v));
-			if (u > N) N = u;
-			if (v > N) N = v;
+			add_edge(u, v, g);
+			++total;
 		}
 		ifs.close();
 	} else {
 		while (getline(cin, line)) {
 			SSCANF((line.c_str(), "%u %u", &u, &v));
-			edges.push_back(Edge(u, v));
-			if (u > N) N = u;
-			if (v > N) N = v;
+			add_edge(u, v, g);
+			++total;
 		}
 	}
-	Graph g(edges.begin(), edges.end(), ++N);
+	return total;
+}
 
-	ItrNeighbour neighbourIt, neighbourEnd;
+void print_adjacency_list(Graph &g) {
+	Graph::adjacency_iterator neighbourIt, neighbourEnd;
 	for (auto v: g.vertex_set()) {
 		cout << v << " --> ";
 		boost::tie(neighbourIt, neighbourEnd) = adjacent_vertices(v, g);
 		for (; neighbourIt != neighbourEnd; ++neighbourIt) cout << *neighbourIt << " ";
 		cout << endl;
 	}
+}
+
+int main(int argc, char* argv[]) {
+	using namespace opt;
+
+	if (chkOption(argv, argv + argc, "-h")) {
+		cout << "bgl-shell -flag [option]" << endl;
+		cout << " -h:\t ask for help" << endl;
+		cout << " -i:\t (cin) input edge list" << endl;
+		cout << " -e:\t perform [BFS|DFS|SSSP], etc." << endl;
+		return 0;
+	}
+
+	char* edges_file = getOption(argv, argv + argc, "-i");
+	char* algorithm  = getOption(argv, argv + argc, "-e");
+
+	Graph g;
+	uint n_edges = get_edges(edges_file, g);
+
+	if (algorithm) {
+		cout << "TODO" << endl;
+	} else print_adjacency_list(g);
 }
