b3e9cec078f6b5321926efb6a907e283222d07d8
ShiZhan <g.shizhan.g@gmail.com>
2015-01-27 17:59:20 +0800

    update trace translating scripts, add one for piping to edge list handlers.

---
+++ scripts/trace-edges.sh
@@ -0,0 +1,2 @@
+export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
+gunzip -c $1 | sed '/BeginHeader/,/EndHeader/d;/^\s\+Disk[Read|Write]/!d' | awk 'BEGIN {FS=","} {print $6}' | $SCRIPT_DIR/../Release/seq-to-edges.exe $2 $3 $4 $5
