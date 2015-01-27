b966928052cb467dbe6f1975429d2fbdf07db988
ShiZhan <g.shizhan.g@gmail.com>
2015-01-27 11:14:02 +0800

    bfs & dfs in bgl

--- src/bgl-shell.cpp
+++ src/bgl-shell.cpp
@@ -2,24 +2,45 @@
 #include <fstream>
 #include <string>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
 #include "options.h"
 #include "utils.h"
 
 using namespace std;
 using namespace boost;
-typedef adjacency_list<setS, vecS, directedS, no_property> Graph;
+typedef adjacency_list<setS, vecS, directedS, no_property> graph_t;
 
-uint get_edges(char* edges_file, Graph &g) {
+class custom_bfs_visitor : public default_bfs_visitor {
+public:
+	template < typename Vertex, typename Graph >
+	void discover_vertex(Vertex v, const Graph &g) const {
+		cout << v << endl;
+	}
+};
+
+class custom_dfs_visitor : public default_dfs_visitor {
+public:
+	template < typename Vertex, typename Graph >
+	void discover_vertex(Vertex v, const Graph &g) const {
+		cout << v << endl;
+	}
+};
+
+uint get_edges(char* edges_file, graph_t &g) {
 	uint u, v, total = 0;
 	string line;
 	if (edges_file) {
 		ifstream ifs(edges_file);
-		while (getline(ifs, line)) {
-			SSCANF((line.c_str(), "%u %u", &u, &v));
-			add_edge(u, v, g);
-			++total;
+		if(ifs) {
+			while (getline(ifs, line)) {
+				SSCANF((line.c_str(), "%u %u", &u, &v));
+				add_edge(u, v, g);
+				++total;
+			}
+			ifs.close();
 		}
-		ifs.close();
 	} else {
 		while (getline(cin, line)) {
 			SSCANF((line.c_str(), "%u %u", &u, &v));
@@ -30,8 +51,8 @@ uint get_edges(char* edges_file, Graph &g) {
 	return total;
 }
 
-void print_adjacency_list(Graph &g) {
-	Graph::adjacency_iterator neighbourIt, neighbourEnd;
+void print_adjacency_list(graph_t &g) {
+	graph_t::adjacency_iterator neighbourIt, neighbourEnd;
 	for (auto v: g.vertex_set()) {
 		cout << v << " --> ";
 		boost::tie(neighbourIt, neighbourEnd) = adjacent_vertices(v, g);
@@ -56,15 +77,20 @@ int main(int argc, char* argv[]) {
 
 	char* edges_file = getOption(argv, argv + argc, "-i");
 	char* algorithm  = getOption(argv, argv + argc, "-e");
-	int   source     = getInt(argv, argv + argc, "-s", SOURCE);
+	uint  source     = getInt(argv, argv + argc, "-s", SOURCE);
 
-	Graph g;
+	graph_t g;
 	uint n_edges = get_edges(edges_file, g);
 
 	if (algorithm) {
-		if (!(strcmp(algorithm, "bfs") && strcmp(algorithm, "BFS"))) { cout << "Do BFS" << endl; }
-		else if (!(strcmp(algorithm, "dfs") && strcmp(algorithm, "DFS"))) { cout << "Do DFS" << endl; }
-		else if (!(strcmp(algorithm, "sssp") && strcmp(algorithm, "SSSP"))) { cout << "Do SSSP" << endl; }
-		else { cout << "Available algorithms: BFS, DFS, SSSP." << endl; }
+		if (!(strcmp(algorithm, "bfs") && strcmp(algorithm, "BFS"))) {
+			custom_bfs_visitor bv;
+			breadth_first_search(g, vertex(source, g), visitor(bv));
+		} else if (!(strcmp(algorithm, "dfs") && strcmp(algorithm, "DFS"))) {
+			custom_dfs_visitor dv;
+			depth_first_search(g, root_vertex(vertex(source, g)).visitor(dv));
+		} else if (!(strcmp(algorithm, "sssp") && strcmp(algorithm, "SSSP"))) {
+			cout << "Do SSSP" << endl;
+		} else { cout << "Available algorithms: BFS, DFS, SSSP." << endl; }
 	} else print_adjacency_list(g);
 }
