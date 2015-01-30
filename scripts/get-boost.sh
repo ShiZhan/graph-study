b659153fff18056f464714121b21f5c0b36db164
ShiZhan <g.shizhan.g@gmail.com>
2015-01-30 16:45:27 +0800

    put boost_dir in Makefile.config

--- scripts/get-boost.sh
+++ scripts/get-boost.sh
@@ -3,4 +3,5 @@ export INSTALL_DIR=$( pwd )
 export BOOST_DIR="$( echo ${INSTALL_DIR}/boost_1_57_0 | sed 's/\//\\\//g')"
 wget http://jaist.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz
 echo "$( tar -xzvf boost_1_57_0.tar.gz | wc -l ) files extracted."
-sed -in-place "s/BOOST_DIR = .*$/BOOST_DIR = ${BOOST_DIR}/g" $SCRIPT_DIR/../src/Makefile
+#sed -in-place "s/BOOST_DIR = .*$/BOOST_DIR = ${BOOST_DIR}/g" $SCRIPT_DIR/../src/Makefile
+echo "BOOST_DIR = ${BOOST_DIR}" > $SCRIPT_DIR/../src/Makefile.config
