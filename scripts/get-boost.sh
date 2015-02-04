8028af0dd95d460f9862b52b7afd983d89d147d3
ShiZhan <g.shizhan.g@gmail.com>
2015-02-04 18:32:36 +0800

    simplify

--- scripts/get-boost.sh
+++ scripts/get-boost.sh
@@ -1,7 +1,7 @@
 export SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 export INSTALL_DIR=$( pwd )
 #export BOOST_DIR="$( echo ${INSTALL_DIR}/boost_1_57_0 | sed 's/\//\\\//g')"
-export BOOST_DIR="$( echo ${INSTALL_DIR}/boost_1_57_0)"
+export BOOST_DIR=${INSTALL_DIR}/boost_1_57_0
 if [ ! -d "boost_1_57_0" ]; then
     echo "Boost not found current directory."
     wget http://jaist.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz
