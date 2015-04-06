6f8c7b9f50644999672dee7a70043b56ccd2592d
ShiZhan <g.shizhan.g@gmail.com>
2015-04-06 16:38:41 +0800

    preserve seperator '^' for randomizing various streams

--- scripts/randomize.sh
+++ scripts/randomize.sh
@@ -1,1 +1,1 @@
-awk 'BEGIN { srand()} {print $0" "int(65535 * rand())}' $@ | sort -n -k3 | awk '{print $1" "$2}'
+awk 'BEGIN { srand() }; {print int(65535 * rand())"^"$0}' $@ | sort -n -t '^' -k 1 | awk 'BEGIN { FS = "^" }; {print $2}'
